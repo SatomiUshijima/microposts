da7756eb9ddd5465f590935baa9b4d73d3ceb2b8
satomiushijima <ushijima0709@e-inv.co.jp>
2016-02-15 11:21:34 +0000

    mod update profile3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :authenticate!, only: [:edit, :update]
 
-
   def show 
    @user = User.find(params[:id])
   end
