cc7905e9f6ac7701525250f4ceeb22670cc97136
satomiushijima <ushijima0709@e-inv.co.jp>
2016-02-16 14:15:11 +0000

    merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
-  def show # 追加
+  before_action :set_user, only: [:show,:edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :authenticate!, only: [:edit, :update]
+
+  def show 
    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -17,11 +21,38 @@ class UsersController < ApplicationController
     end
   end
 
+  # 基本情報の編集
+  def edit
+  end
+  
+  # 更新処理
+  def update
+    if @user.update(user_params)
+      flash[:seccess] = "Successfully updated"
+    # 保存に成功した場合はプロフィールへリダイレクト
+      redirect_to @user
+    else
+      flash[:alert] = "Updating Failed"
+      render 'edit'
+    end
+  end
+  
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                :password_confirmation, :location, :profile)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+# check current_user is editing self ?
+  def authenticate!
+    if @user != current_user
+      redirect_to root_url, flash: { alert: "不正なアクセス" }
+    end
+  end
 end
+ 
